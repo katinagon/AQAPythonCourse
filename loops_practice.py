056eed45ec5543238602ed81ba41cd559481ec12
katinagon <katinagon@yandex.ru>
2026-05-13 08:36:28 +0300

    feat: add practice 3

--- loops_practice.py
+++ loops_practice.py
@@ -1,3 +1,7 @@
+import random
+import time
+
+
 class TaskSolution:
     @staticmethod
     def print_numbers_up_to_5():
@@ -12,3 +16,14 @@ class TaskSolution:
         words = [f"str{i}" for i in range(10)]
         for word in words:
             print(word)
+
+
+class Rostics:
+    @staticmethod
+    def check_load():
+        i = 10
+        while i > 0:
+            load = random.randint(0, 100)
+            print(f"Load: {load}%", "WARNING!" if load > 85 else "OK")
+            time.sleep(0.2)
+            i -= 1
